f53fe98444dde544d914f3a9b27f5c8fa0ee9981
rhuanaa <rhuanandrey@gmail.com>
2026-05-14 21:20:16 -0300

    fix(server): revert to standard TanStack Start handler for local dev compatibility

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import { createStartHandler, defaultRenderHandler } from "@tanstack/react-start/server";
 import { getRouter } from "./router";
 
-const handler = createStartHandler({
+export default createStartHandler({
   createRouter: getRouter,
   renderHandler: defaultRenderHandler,
 });
-
-export default handler;
